ed49d763ba6bcb9045e0012c77babea90744a83d
SorerBOT <alonfiller@gmaill.com>
2025-11-23 19:19:48 +0200

    (Feat) added type annotations, __str__

--- ex1.py
+++ ex1.py
@@ -5,44 +5,60 @@ import utils
 id = ["No numbers - I'm special!"]
 
 
+KEY_SIZE    = "Size"
+KEY_WALLS   = "Walls"
+KEY_TAPS    = "Taps"
+KEY_PLANTS  = "Plants"
+KEY_ROBOTS  = "Robots"
+
 class State:
-    KEY_SIZE    = "Size"
-    KEY_WALLS   = "Walls"
-    KEY_TAPS    = "Taps"
-    KEY_PLANTS  = "Plants"
-    KEY_ROBOTS  = "Robots"
     size        = (0,0) 
     walls       = frozenset()
+    taps:       dict[tuple, int]
+    plants:     dict[tuple, int]
+    robots:     dict[int, tuple]
 
     @staticmethod
     def create_initial_state(initial):
-        State.size       = initial[State.KEY_SIZE]
-        State.walls      = frozenset(initial[State.KEY_WALLS])
+        State.size       = tuple(initial[KEY_SIZE])
+        State.walls      = frozenset(initial[KEY_WALLS])
 
-        return State(initial[State.KEY_TAPS],
-                     initial[State.KEY_PLANTS],
-                     initial[State.KEY_ROBOTS])
+        return State(initial[KEY_TAPS],
+                     initial[KEY_PLANTS],
+                     initial[KEY_ROBOTS])
         
     def __init__(self, taps, plants, robots):
-        self.taps       = taps
-        self.plants     = plants
-        self.robots     = robots
+        self.taps       = dict(taps)
+        self.plants     = dict(plants)
+        self.robots     = dict(robots)
+
+    def __str__(self):
+        str_size    = f"Grid dimensions: {State.size}"
+        str_walls   = f"Walls coordinates: {State.walls}"
+        str_taps    = f"Taps: {self.taps}"
+        str_plants    = f"Plants: {self.plants}"
+        str_robots    = f"Robots: {self.robots}"
+
+        return f"{str_size}\n{str_walls}\n{str_taps}\n{str_plants}\n{str_robots}"
 
 class WateringProblem(search.Problem):
     """This class implements a pressure plate problem"""
+    state: State
 
     def __init__(self, initial):
         """ Constructor only needs the initial state.
         Don't forget to set the goal or implement the goal test"""
         search.Problem.__init__(self, initial)
+        self.state = State.create_initial_state(initial)
+        print ( str(self.state) )
 
     def successor(self, state):
         """ Generates the successor states returns [(action, achieved_states, ...)]"""
         utils.raiseNotDefined()
 
-    def goal_test(self, state):
+    def goal_test(self, state: State):
         """ given a state, checks if this is the goal state, compares to the created goal state returns True/False"""
-        utils.raiseNotDefined()
+        return state.plants
 
     def h_astar(self, node):
         """ This is the heuristic. It gets a node (not a state)
