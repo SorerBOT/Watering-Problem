123e85c12a5740faa4c1f7be0f27c6c8cef6fd79
SorerBOT <alonfiller@gmaill.com>
2025-11-29 12:07:09 +0200

    (Feat) asasdasdasd

--- ex1.py
+++ ex1.py
@@ -12,12 +12,13 @@ KEY_PLANTS  = "Plants"
 KEY_ROBOTS  = "Robots"
 
 class State:
-    size:       tuple[int, int]
-    walls:      dict[tuple[int, int], bool]
-    taps:       dict[tuple[int, int], int]
-    plants:     dict[tuple[int, int], int]
-    robots:     dict[tuple[int, int], tuple[str, int, int]]
-    last_actions: list[tuple[str, str]]
+    size:           tuple[int, int]
+    walls:          dict[tuple[int, int], bool]
+    taps:           dict[tuple[int, int], int]
+    plants:         dict[tuple[int, int], int]
+    robots:         dict[tuple[int, int], tuple[str, int, int]]
+    last_actions:   list[tuple[str, str]]
+    last_states:    list[int]
 
     @staticmethod
     def create_initial_state(initial):
@@ -32,11 +33,13 @@ class State:
                     _taps: dict[tuple, int],
                     _plants: dict[tuple, int],
                     _robots: dict[tuple, tuple],
-                    _last_actions: list[tuple[str, str]] | None = None):
+                    _last_actions: list[tuple[str, str]] = [],
+                    _last_states: list[int] = []):
         self.taps           = dict(_taps)
         self.plants         = dict(_plants)
         self.robots         = dict(_robots)
-        self.last_actions   = list(_last_actions or [])
+        self.last_actions   = list(_last_actions)
+        self.last_states    = list(_last_states)
 
     def __str__(self):
         str_size            = f"Grid dimensions: {State.size}"
@@ -46,6 +49,20 @@ class State:
         str_robots          = f"Robots: {self.robots}"
 
         return f"{str_size}\n{str_walls}\n{str_taps}\n{str_plants}\n{str_robots}"
+
+    def __hash__(self):
+        return hash((
+            tuple(sorted(self.walls.items())),
+            tuple(sorted(self.taps.items())),
+            tuple(sorted(self.plants.items())),
+            tuple(sorted(self.robots.items())),
+            ))
+    def __eq__(self, other):
+        return (
+            self.walls  == other.walls  and
+            self.taps   == other.taps   and
+            self.plants == other.plants and
+            self.robots == other.robots)
 
 def last_actions_append(last_actions: list[tuple[str, str]],
                  action: tuple[str, str]):
@@ -79,9 +96,9 @@ class WateringProblem(search.Problem):
                 and not State.walls.get((i,j), False)
                 and not state.robots.get((i,j), False))
 
-        # ---------------------------------------------------------------------
-        # Didn't want to commit to a specific format for saving the robots.
-        # ---------------------------------------------------------------------
+        # -------------------------------------------------------------------
+        # Didn't want to commit to a specific format for saving the robots. #
+        # -------------------------------------------------------------------
         generate_robot          = lambda id, i, j, load, capacity: (id, load, capacity)
         generate_robot_key      = lambda id, i, j, load, capacity: (i,j)
 
@@ -92,6 +109,8 @@ class WateringProblem(search.Problem):
 
                 del state_new.robots[generate_robot_key(id, i, j, load, capacity)]
                 state_new.robots[generate_robot_key(id, i+1, j, load, capacity)] = generate_robot(id, i+1, j, load, capacity)
+
+                # need to append the hash to last_states then check for equality and prune
                 last_actions_update(state_new.last_actions, ('D', id))
                 moves.append((f"DOWN{{{id}}}", state_new))
 
@@ -143,7 +162,7 @@ class WateringProblem(search.Problem):
                         del state_new.robots[generate_robot_key(id, i, j, load, capacity)]
                         state_new.robots[generate_robot_key(id, i, j, load + 1, capacity)] = generate_robot(id, i, j, load + 1, capacity)
                         state_new.taps[(i,j)] = water_available - 1
-                        last_actions_update(state_new.last_actions, ('L', id))
+                        last_actions_update(state_new.last_actions, ('A', id))
                         moves.append((f"LOAD{{{id}}}", state_new))
         return moves
 
@@ -154,15 +173,44 @@ class WateringProblem(search.Problem):
     def h_astar(self, node):
         """ This is the heuristic. It gets a node (not a state)
         and returns a goal distance estimate"""
-        return (2 * sum(node.state.plants.values())
-                - sum(load for (id, load, capacity) in node.state.robots.values()))
+        min_distance_to_tap     = float('inf')
+        min_distance_to_plant   = float('inf')
+        total_load              = 0
+        for (i, j), (id, load, capacity) in node.state.robots.items():
+            current_distance_to_tap     = min((abs(i - tap_i) + abs(j - tap_j)) for (tap_i, tap_j) in node.state.taps.keys())
+            current_distance_to_plant   = min((abs(i - plant_i) + abs(j - plant_j)) for (plant_i, plant_j) in node.state.plants.keys())
+            min_distance_to_tap         = min(min_distance_to_tap, current_distance_to_tap)
+            min_distance_to_plant       = min(min_distance_to_plant, current_distance_to_plant)
+            total_load                  += load
+
+        heuristic                       = (2 * sum(node.state.plants.values()) - total_load)
+
+        if total_load == 0:
+            heuristic                   += min_distance_to_tap
+        else:
+            heuristic                   += min(min_distance_to_tap, min_distance_to_plant)
+        return heuristic
 
     def h_gbfs(self, node):
         """ This is the heuristic. It gets a node (not a state)
         and returns a goal distance estimate"""
-        return (2 * sum(node.state.plants.values())
-                - sum(load for (id, load, capacity) in node.state.robots.values()))
+        min_distance_to_tap     = float('inf')
+        min_distance_to_plant   = float('inf')
+        total_load              = 0
+        for (i, j), (id, load, capacity) in node.state.robots.items():
+            current_distance_to_tap     = min((abs(i - tap_i) + abs(j - tap_j)) for (tap_i, tap_j) in node.state.taps.keys())
+            current_distance_to_plant   = min((abs(i - plant_i) + abs(j - plant_j)) for (plant_i, plant_j) in node.state.plants.keys())
+            min_distance_to_tap         = min(min_distance_to_tap, current_distance_to_tap)
+            min_distance_to_plant       = min(min_distance_to_plant, current_distance_to_plant)
+            total_load                  += load
 
+        heuristic                       = (2 * sum(node.state.plants.values()) - total_load)
+
+        if total_load == 0:
+            heuristic                   += min_distance_to_tap
+        else:
+            heuristic                   += min(min_distance_to_tap, min_distance_to_plant)
+        return heuristic
 
 def create_watering_problem(game):
     print("<<create_watering_problem")
